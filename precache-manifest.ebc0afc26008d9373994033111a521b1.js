631d00ce57a0ce3e36cbb2abce66175d8475f41c
igor <igor.homechko@gmail.com>
2022-05-21 11:16:29 +0700

    Updates

---
+++ precache-manifest.ebc0afc26008d9373994033111a521b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37b481713cfd00b4668",
+    "url": "/kozhindev-test-exchanges/static/css/main.32f61867.chunk.css"
+  },
+  {
+    "revision": "a37b481713cfd00b4668",
+    "url": "/kozhindev-test-exchanges/static/js/main.cdfc6454.chunk.js"
+  },
+  {
+    "revision": "e60f46550e27d693ba04",
+    "url": "/kozhindev-test-exchanges/static/js/runtime~main.50bb563e.js"
+  },
+  {
+    "revision": "68743ec08d87aef5974a",
+    "url": "/kozhindev-test-exchanges/static/css/2.bd09386f.chunk.css"
+  },
+  {
+    "revision": "68743ec08d87aef5974a",
+    "url": "/kozhindev-test-exchanges/static/js/2.5a3b47d8.chunk.js"
+  },
+  {
+    "revision": "b55eb6406ae7e5e28a90fe1d16dc4045",
+    "url": "/kozhindev-test-exchanges/index.html"
+  }
+];
